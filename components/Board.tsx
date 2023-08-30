4480aeee42fabbecbfe4c018b214fb57ad3d581b
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-08-30 16:43:11 +0500

    add beautiful dnd structure

---
+++ components/Board.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => <div></div>}
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+export default Board;
