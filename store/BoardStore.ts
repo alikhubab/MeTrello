07e918abc8298e27f6a80496ec9c7985e830d4bc
khubaib-ali-codegenio <117726252+khubaib-ali-codegenio@users.noreply.github.com>
2023-09-03 15:28:56 +0500

    transform totods data comming from the backend

---
+++ store/BoardStore.ts
@@ -0,0 +1,16 @@
+import { getTodosGroupedByColumns } from "@/lib/getTodosGroupedByColumns";
+import { Board, Column, TypedColumn } from "@/typings";
+import { create } from "zustand";
+
+interface BoardState {
+  board: Board;
+  getBoard: () => void;
+}
+
+export const useBoardStore = create<BoardState>((set) => ({
+  board: { columns: new Map<TypedColumn, Column>() },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumns();
+    set({ board });
+  },
+}));
---
+++ typings.d.ts
@@ -0,0 +1,25 @@
+import { Models } from "appwrite";
+
+interface Board {
+  columns: Map<typedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inProgress" | "done";
+
+interface Column {
+  id: TypedColumn;
+  todos: Todo[];
+}
+
+export interface Todo extends Partial<Models.Document> {
+  $id: string;
+  title: string;
+  status: TypedColumn;
+  $createdAt: string;
+  image?: Image;
+}
+
+interface Image {
+  buckedId: string;
+  fileId: string;
+}
